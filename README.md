1876d184582dbf885b92ffee551e76440ca051d4
subra <subra@orangebrush.net>
2018-12-15 23:15:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# timed-cache
+
+A simple elm module to help manage remote data that is accessed more often
+than it is fetched.
+
+Say you have a task that fetches the number of pending notifications via a http
+call. You want to display the pending numbers in a badge on your navbar but don't
+want to make the http call with every navbar page reload. You also want to ensure
+that this is updated every 1 min to ensure you are showing the correct number to
+your users.
+
+A contrived example of how to use this module with the scenerio above:
+
+```elm
+import TimedCache as TC
+
+fetchPendingNotificationCount : Task Http.Error Int
+fetchPendingNotificationCount = ...
+
+-- We first init the model that we need:
+(initModel, initCmd) =
+  TC.init
+    fetchPendingNotificationCount
+    (1000 * 60) -- Fetch a new value every 1 minute
+    0 -- The initial count of notifications should be 0
+
+-- Our applications' model
+type alias Model =
+  { notifications : TC.Model Http.Error Int
+  , ... -- Other fields of your model
+  }
+
+type Msg =
+    NoOp
+  | NotificationMsg (TC.Msg Http.Error Int)
+
+update : Model -> Msg -> (Model, Cmd Msg)
+update model msg =
+  case msg of
+    NoOp -> (model, Cmd.none)
+    NotificationMsg subMsg ->
+      let (m, c) = TC.update model.notifications subMsg
+      in ({model | notifications = m}, Cmd.map NotificationMsg c)
+
+sub : Model -> Sub Msg
+sub model =
+  Sub.batch
+    [ TC.sub model.notifications
+    , ... -- Other subscriptions you may need
+    ]
+
+view : Model -> Html Msg
+view model =
+  let notificationCount = String.fromInt model.notifications.value
+  in div [] [ text <| "Number of notifications: " ++ notificationCount ]
+
+```
---
+++ src/TimedCache.elm
@@ -0,0 +1,79 @@
+module TimedCache exposing (Model,init, update, sub)
+
+{-| A module to help manage accessing values that are obtained remotely and are accessed more often than they are fetched
+
+An example of this is maybe the notification count on your social feed which
+is a value you would need to fetch over HTTP and need in multiple places
+but might not want to fetch as often as you use it.
+
+@docs Model,init,update,sub
+-}
+
+import Time exposing (Posix)
+import Task exposing (Task)
+
+{-|-}
+type alias Model e a =
+  { value : Result e a
+  -- ^ The current value of the cache.
+  , maxAge : Int
+  -- ^ How old can the value be before considered stale. In ms
+  , lastFetched : Maybe Posix
+  -- ^ When was the last time this data was fetched. Nothing if unknown/never fetched
+  , fetch : Task e a
+  -- ^ The action to fetch the value needed. Modelled as a maybe since the action
+  --   can fail. If it fails, the old value is held.
+  }
+
+
+{-| Create the initial model and cmds
+
+The arguments in order are:
+  + The task to fetch your value
+  + The maxAge; how old can a value get before needing to be refreshed.
+  + An initial value
+-}
+init : Task e a -> Int -> a -> (Model e a, Cmd (Msg e a))
+init task maxAge a =
+  let
+    model =
+      { value = Ok a
+      , maxAge = maxAge
+      , lastFetched = Nothing
+      , fetch = task
+      }
+    cmd = fetchValue model
+  in
+    (model, cmd)
+
+type Msg e a =
+    Tick Posix
+  | NewValue (Result e a)
+
+{-|-}
+update : Model e a -> Msg e a -> (Model e a, Cmd (Msg e a))
+update model msg =
+  case msg of
+    Tick now -> handleTick model now
+    NewValue newValue -> handleNewValue model newValue
+
+handleTick : Model e a -> Posix -> (Model e a, Cmd (Msg e a))
+handleTick model now =
+  let shouldFetch =
+        case model.lastFetched of
+          Just lastFetchedTime -> Time.posixToMillis now >= Time.posixToMillis lastFetchedTime + model.maxAge
+          Nothing -> True
+      cmd = if shouldFetch then fetchValue model else Cmd.none
+      newModel = if shouldFetch then { model | lastFetched = Just now } else model
+  in (newModel, cmd)
+
+handleNewValue : Model e a -> Result e a -> (Model e a, Cmd (Msg e a))
+handleNewValue model newValue =
+  ({model | value = newValue }, Cmd.none)
+
+fetchValue : Model e a -> Cmd (Msg e a)
+fetchValue model = Task.attempt NewValue model.fetch
+
+{-|-}
+sub : Model e a -> Sub (Msg e a)
+sub model = Time.every (toFloat model.maxAge) Tick
